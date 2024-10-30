bc12c465c8c8e7188c09b07a4ee0fb94e204c7f4
debarshee2004 <debarsheechakraborty.11d@gamil.com>
2024-10-30 20:13:55 +0530

    Add AuthController and AuthService to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
